1bc5d7604b09b38eb053f09e872e7d885558b092
MontejoJorge <jorge.montejo@ikasle.egibide.org>
2020-11-13 13:27:50 +0100

    remove email regex

--- js/login.js
+++ js/login.js
@@ -61,10 +61,7 @@ function validateForm(type) {
             if (!apellidoRegex.test($("#apellido"))) {
                 throw "El apellido no es valido";
             }
-            var emailRegex = new RegExp("^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$");
-            if (!emailRegex.test($("#email"))) {
-                throw "El email no es valido";
-            }
+            //el regex del email lo valida el html
             return true;
         }
     } catch (error) {
