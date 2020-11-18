28f784c91d01a8fcab4d58bb1d480a879d69b24d
unknown <2gdaw11@ARRIAGAINFO.LOCAL>
2020-11-18 10:56:14 +0100

    ajax username and email

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     buttonsChangeForm();
-    checkUsernameEmail();
+    checkAvailability("Username");
+    checkAvailability("Email");
 });
 
 function buttonsChangeForm() {
@@ -12,12 +13,13 @@ function buttonsChangeForm() {
             $("#login").css("display", "none");
             $("#signup").css("display", "flex");
             signUpSingInstatus = true;
-
+            $(".username").eq(0).focus();
         } else {
             //ocultar uno de los formularios y mostrar el otro
             $("#signup").css("display", "none");
             $("#login").css("display", "flex");
             signUpSingInstatus = false;
+            $(".username").eq(1).focus();
         }
         //limpio los campos de los formularios
         clearForms();
@@ -42,10 +44,10 @@ function validateForm(type) {
                 throw "El nombre de usuario tiene un formato incorrecto";
             }
             if (passwordRegex.test(password)) {
-                if (password!=$("#password2").val()){
+                if (password != $("#password2").val()) {
                     throw "Las contraseñas no coinciden";
                 }
-            }else {
+            } else {
                 throw "La contraseña debe contener 8 caracteres, una mayuscula, una minuscula, un numero y un caracter especial (@#$%&?¿!¡._-)";
             }
             var nombreRegex = new RegExp("^(([a-zA-Z ])?[a-zA-Z]*){1,3}$");
@@ -57,8 +59,8 @@ function validateForm(type) {
                 throw "El apellido tiene un formato incorrecto";
             }
             var emailRgex = new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$");
-            if (!emailRgex.test($("#email").val())){
-                throw "El email tiene un formato incorrecto"
+            if (!emailRgex.test($("#email").val())) {
+                throw "El email tiene un formato incorrecto";
             }
             return true;
         }
@@ -71,19 +73,68 @@ function clearForms() {
     $("form")[0].reset();
     $("form")[1].reset();
 }
-function checkUsernameEmail(){
-    $(".username").eq(1).on("input", function(){
-        var tmp_username = $(".username").eq(1).val().trim();
-
-        if (tmp_username!=""){
-        $.ajax({
-            type: "post",
-            url: "../php/ajax.php",
-            data: {action: "checkUsername", value: tmp_username},
-            success: function (response) {
-                alert(response);
-            }
-        });
+function checkAvailability(variable) {
+    switch (variable) {
+        case "Username":
+            checkUsername();
+            break;
+        case "Email":
+            checkEmail();
+            break;
     }
-})
+}
+function checkUsername() {
+    var usernameRegex = new RegExp("^(?=[a-zA-Z0-9._-]{3,16}$)(?!.*[_.-]{2})[^_.-].*[^_.-]$");
+    $(".username").eq(1).on("input", function () {
+        var username = $(".username").eq(1).val();
+        if (usernameRegex.test(username)) {
+            if (username != "") {
+                $.ajax({
+                    type: "post",
+                    url: "../php/ajax.php",
+                    data: { action: "checkUsername", value: username },
+                    success: function (response) {
+                        if (response == 1) {
+                            $(".fa-user-circle").css("color", "red");
+                            $(".fa-user-circle").css("border-bottom", "1px solid red");
+                        } else {
+                            $(".fa-user-circle").css("color", "green");
+                            $(".fa-user-circle").css("border-bottom", "1px solid green");
+                        }
+                    }
+                });
+            }
+        } else {
+            $(".fa-user-circle").css("color", "#ffb600");
+            $(".fa-user-circle").css("border-bottom", "1px solid #ffb600");
+        }
+    })
+}
+function checkEmail() {
+    var emailRgex = new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$");
+    $("#email").on("input", function () {
+        var email = $("#email").val();
+        if (emailRgex.test(email)) {
+            if (email != "") {
+                $.ajax({
+                    type: "post",
+                    url: "../php/ajax.php",
+                    data: { action: "checkEmail", value: email },
+                    success: function (response) {
+                        if (response == 1) {
+                            $(".fa-at").css("color", "red");
+                            $(".fa-at").css("border-bottom", "1px solid red");
+                        } else {
+                            $(".fa-at").css("color", "green");
+                            $(".fa-at").css("border-bottom", "1px solid green");
+                        }
+                    }
+                });
+            }
+        }
+        else {
+            $(".fa-at").css("color", "#ffb600");
+            $(".fa-at").css("border-bottom", "1px solid #ffb600");
+        }
+    })
 }
