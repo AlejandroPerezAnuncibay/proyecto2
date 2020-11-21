0ca5fe70ade29b1391267d5e98a9b128e9a56f0e
MontejoJorge <jorgemon.lopez@gmail.com>
2020-11-21 12:14:33 +0100

    regex modificar usuario

--- js/user.js
+++ js/user.js
@@ -64,7 +64,6 @@ function checkAvailability(variable){
                 }
             });
         } else {
-            $("#submit").attr("disabled", true);
             $(".check-"+field).css("display", "none");
             $(".times-"+field).css("display", "none");
         }
@@ -80,33 +79,59 @@ function regexPassword(password, password2){
             return true;
         }
     } else {
-
         throw "La contraseña debe contener 8 caracteres, una mayuscula, una minuscula, un numero y un caracter especial (@#$%&?¿!¡._-)";
-
     }
 }
 function validarCampos(){
     try {
-//TODO regex de todos los campos
-//https://stackoverflow.com/questions/43157936/javascript-import-function-syntax
-
-
-
-
-
+        regexUsername($("#nombreUsuario").val());
+        regexName($("#nombre").val());
+        regexSurname($("#apellido").val());
+        regexEmail($("#email").val());
     } catch (error){
-        alert(error);
+        $("#errorModificar").empty();
+        $("#errorModificar").append(error);
         return false;
     }
 }
 function validarContrasenas() {
     try {
         regexPassword($("#pass1").val(),$("#pass2").val());
-
         return true;
-
     } catch (error) {
         alert(error);
         return false;
+    }
+}
+function regexUsername(username){
+    var usernameRegex = new RegExp("^(?=[a-zA-Z0-9._-]{3,16}$)(?!.*[_.-]{2})[^_.-].*[^_.-]$");
+    if (!usernameRegex.test(username)) {
+        throw "El nombre de usuario tiene un formato incorrecto";
+    } else {
+        return true;
+    }
+}
+function regexName(nombre) {
+    var nombreRegex = new RegExp("^(([a-zA-Z ])?[a-zA-Z]*){1,3}$");
+    if (!nombreRegex.test(nombre)) {
+        throw "El nombre tiene un formato incorrecto";
+    } else {
+        return true;
+    }
+}
+function regexSurname(surname){
+    var apellidoRegex = new RegExp("^(([a-zA-Z ])?[a-zA-Z]*){1,4}$");
+    if (!apellidoRegex.test(surname)) {
+        throw "El apellido tiene un formato incorrecto";
+    } else {
+        return true;
+    }
+}
+function regexEmail(email){
+    var emailRgex = new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$");
+    if (!emailRgex.test(email)) {
+        throw "El email tiene un formato incorrecto";
+    } else {
+        return true;
     }
 }
